fee34d03938bfa9d9cae05e306e791a728efc17b
Louis Choinière <louis.choiniere@hotmail.com>
2021-01-21 14:06:40 -0500

    :construction: wip login controller

---
+++ PrivateNotes/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using PrivateNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrivateNotes.Controllers {
+	class LoginController {
+
+		public static void Login(LoginCredentials loginCredentials) {
+
+			if (loginCredentials.isValid()) {
+				// close login page
+				// start notes page
+			} else {
+				// show invalid password message
+			}
+		}
+	}
+}
---
+++ PrivateNotes/Models/LoginCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrivateNotes.Models {
+	class LoginCredentials {
+		public Boolean isValid() {
+
+			// check credentials
+			return false;
+		}
+	}
+}
