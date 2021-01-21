091454a2985a4bc21601484c4304ac84ff9ff120
Simon St-Andre <simonstandre@gmail.com>
2021-01-21 14:08:57 -0500

    ✨ Login form now has things in it

---
+++ PrivateNotes/LoginForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace PrivateNotes {
+	partial class Login {
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.createAccoutButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.passwordInput);
+            this.panel1.Controls.Add(this.usernameInput);
+            this.panel1.Controls.Add(this.passwordLabel);
+            this.panel1.Controls.Add(this.usernameLabel);
+            this.panel1.Controls.Add(this.createAccoutButton);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(907, 461);
+            this.panel1.TabIndex = 0;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.title.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.title.Location = new System.Drawing.Point(242, 30);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(406, 36);
+            this.title.TabIndex = 0;
+            this.title.Text = "Welcome To Private Notes!!";
+            // 
+            // createAccoutButton
+            // 
+            this.createAccoutButton.Location = new System.Drawing.Point(753, 21);
+            this.createAccoutButton.Name = "createAccoutButton";
+            this.createAccoutButton.Size = new System.Drawing.Size(135, 66);
+            this.createAccoutButton.TabIndex = 69;
+            this.createAccoutButton.Text = "Create Account";
+            this.createAccoutButton.UseVisualStyleBackColor = true;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.usernameLabel.Location = new System.Drawing.Point(269, 161);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(105, 23);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordLabel.Location = new System.Drawing.Point(270, 217);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(104, 23);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // usernameInput
+            // 
+            this.usernameInput.Location = new System.Drawing.Point(415, 161);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(195, 23);
+            this.usernameInput.TabIndex = 1;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(415, 221);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(195, 23);
+            this.passwordInput.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(361, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Log In!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 498);
+            this.Controls.Add(this.panel1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button createAccoutButton;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
